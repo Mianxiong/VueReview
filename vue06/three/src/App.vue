94407f9ce8a5d7e3ce0981be273ca69d49e796aa
hmx <757134184@qq.com>
2020-04-26 22:58:08 +0800

    vue

--- vue06/three/src/App.vue
+++ vue06/three/src/App.vue
@@ -3,42 +3,65 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <Tab :title="['aa','bb','cc']" :content="['aa的内容','bb的内容','cc的内容']"></Tab> -->
-    <Demo @msg="receive">
-      <template #default><!--匿名插槽名字-->
+    
+    <!-- <Demo @msg="receive">
+      <template #default>
         <span>用户名</span>
       </template>
       <template v-slot:btn="{str}">
         <button @click="add(str)">add</button>
       </template>
     </Demo>
-    {{list}}
+    {{list}} -->
+  {{goodsList}}{{usersList}}
+    <Hw :title="['用户','商品']" :goods="goodsList" :users="usersList"></Hw>
+    <!-- {{goodsList}}
+    {{usersList}} -->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Tab from './components/Tab.vue'
-import Demo from './components/Demo.vue'
+// import Demo from './components/Demo.vue'
+import Hw from './components/Hw.vue'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
     // HelloWorld
     // Tab
-    Demo
+    // Demo
+    Hw
   },
   methods:{
-    receive(s){
-      this.list.push(s)
-    },
+    // receive(s){
+    //   this.list.push(s)
+    // },
     add(s){
       //插槽中的按钮事件处理
       this.receive(s)
     }
   },
+  
   data(){
     return {
-      list:[]
+      goodsList:[],
+      usersList:[]
     }
+  },
+  created(){
+    
+      axios.get("http://rap2.taobao.org:38080/app/mock/226329/api/goods").then((res)=>{
+      // console.log(res.data.data.list)
+      this.goodsList = res.data.data.list
+    })
+    axios.get("http://rap2.taobao.org:38080/app/mock/226329/api/users").then((res1)=>{
+      this.usersList = res1.data.data.list
+      // console.log(res1.data.data.list)
+    })
+   
+    
   }
 }
 </script>
---
+++ vue06/three/src/components/Hw.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="title">
+            <span v-for="(item,index) in title" :key="index">{{item}}</span>
+        </div>
+        <!-- {{content}} -->
+        <component :is="coms[curIndex]" class="box"></component>
+    </div>
+    
+</template>
+<script>
+    export default{
+        props:['title','goods','users'],
+        data(){
+            return{
+                coms:[],
+                curIndex:0,
+                content:[this.goods,this.users]
+            }
+        },
+        
+        created(){
+            console.log(this.title)
+            console.log(this.goods)
+            console.log(this.users)
+            this.coms = this.content.map((item)=>{
+                return {template:`<div>${item}</div>`}
+            })
+        }
+    }
+</script>
+<style>
+    .title{
+        /* width: 300px; */
+        height: 30px;
+    }
+    .title span{
+
+        line-height: 30px;
+        border:1px solid #ccc;
+    }
+
+</style>
